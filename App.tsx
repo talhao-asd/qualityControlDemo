08d57e82479dafcbd02848a51d2b663d7167b790
talhao-asd <talhao.asd@gmail.com>
2025-01-27 17:48:15 +0300

    feat: Deleted necessary lines

--- App.tsx
+++ App.tsx
@@ -1,13 +1,9 @@
-import { View, Text } from 'react-native'
 import React from 'react'
-import Header from './src/components/Header'
 import HomeScreen from './src/screens/HomeScreen'
 
 const App = () => {
   return (
-    <>
     <HomeScreen />
-    </>
   )
 }
 
