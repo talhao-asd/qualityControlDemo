c28e7c59f67e72e2743492494aecbf7c32a462a7
talhaonder <ltalhaonderl@gmail.com>
2025-02-05 18:03:10 +0300

    feat: Configure Android MainActivity for React Native
    - Set up MainActivity class extending ReactActivity
    - Configure main component name as "QualityControlDemo"
    - Implement ReactActivityDelegate with New Architecture support
    - Override onCreate method with null savedInstanceState
    - Enable Fabric renderer through DefaultNewArchitectureEntryPoint

--- android/app/src/main/java/com/qualitycontroldemo/MainActivity.kt
+++ android/app/src/main/java/com/qualitycontroldemo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.qualitycontroldemo
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -18,4 +19,8 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
 }
