57b36a629ca2ac1267f52f21ca645dcee9bf2e4f
talhaonder <ltalhaonderl@gmail.com>
2025-02-13 16:21:49 +0300

    feat: integrate React Navigation with multiple screens
    - Integrated NavigationContainer and createNativeStackNavigator into the App component.
    - Added new screens: Istanbul, AllProducts, and ListScreen alongside Home.
    - Set "Istanbul" as the initial route for the navigator.
    - Wrapped the NavigationContainer with the Redux Provider to maintain state management.

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
+import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './src/store';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/HomeScreen';
+import IstanbulScreen from './src/screens/IstanbulScreen';
+import AllProducts from './src/screens/AllProducts';
+import ListScreen from './src/screens/ListScreen';
+
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <Provider store={store}>
-        <HomeScreen />
+      <NavigationContainer>
+        <Stack.Navigator 
+          initialRouteName="Istanbul"
+          screenOptions={{
+            headerShown: false
+          }}
+        >
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Istanbul" component={IstanbulScreen} />
+          <Stack.Screen name="AllProducts" component={AllProducts} />
+          <Stack.Screen name="ListScreen" component={ListScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 };
