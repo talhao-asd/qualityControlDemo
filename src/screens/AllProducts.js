9aaa26b606b51f6a67008451abbdf5c761713095
talhaonder <ltalhaonderl@gmail.com>
2025-02-13 16:29:49 +0300

    new libraries

---
+++ src/screens/AllProducts.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AllProducts = () => {
+  return (
+    <View>
+      <Text>AllProducts</Text>
+    </View>
+  )
+}
+
+export default AllProducts
